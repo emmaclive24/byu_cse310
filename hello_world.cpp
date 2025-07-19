202bb6df211ab81d43dc71993ed79d9973851865
Clive Tumbare <131152728+emmaclive24@users.noreply.github.com>
2025-07-19 09:03:24 +0200

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
-
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
