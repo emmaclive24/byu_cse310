3914fb35427f789357c84a4499c5d0111169321d
Clive Tumbare <131152728+emmaclive24@users.noreply.github.com>
2025-07-19 09:10:09 +0200

    update

--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl; // Print the message to the console
-    cout << "Press any key to terminate the program..." << endl; // Prompt the user
-    cin.get(); // Wait for user input
-    return 0; // Indicate successful execution
-}
